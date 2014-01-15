7b541c46a597a109bd6bffd0db5a93595f09e7b6
nope <chuck_testa>
2014-01-15 17:35:17 -0500

    Four questions done

---
+++ exercise7.rb
@@ -0,0 +1,27 @@
+#Question 1
+students = {
+  :cohort1 => 34,
+  :cohort2 => 42,
+  :cohort3 => 22
+}
+
+def student_list(students)
+  students.each do |key, value|
+    puts "#{key}: #{value} students"
+  end 
+end 
+
+student_list(students)
+
+#Question 2
+students[:cohort4] = 43
+puts students
+
+#Question 4
+students.each do |key, value|
+  students[key] = (value * 1.05).round
+end
+
+puts students
+
+#Question 5
